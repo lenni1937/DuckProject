5587c8bc2b45bfa677956305433b9da744b83e91
dmitryammosov <dmitryammosov@gmail.com>
2015-03-16 23:04:56 +0900

    Update Duck.cs

--- Duck.cs
+++ Duck.cs
@@ -13,8 +13,15 @@ namespace DuckProject {
         fly = false;
         swim = false;
         }
-        public Duck(bool fly, bool swim)
+        public Duck(bool _fly, bool _swim)
         {
+            if (_fly && !_swim || !_fly && swim) {
+                fly = _fly;
+                swim = _swim;
+            }
+            else {
+                Console.WriteLine("Invalid input data");
+            }
         }
         sealed public void fly()
         {
@@ -26,6 +33,5 @@ namespace DuckProject {
             fly = false;
             swim = true;
         }
-        public void 
     }
 }
